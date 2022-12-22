e77559c81172253dd78556d77503679ced633d50
amorimcode <bruno.amorim032@gmail.com>
2022-12-21 22:51:31 -0300

    tripe A

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -2,8 +2,23 @@ import Category from "./category";
 
 describe("Category test", () => {
   test("constructor of category", () => {
-    const category = new Category("Movie");
+    // Triple AAA - Arrange, Act, Assert
 
+    //  Arrange
+    const props = {
+      name: "Movie",
+      description: "Movie description",
+      isActive: true,
+      createdAt: new Date(),
+    };
+
+    //  Act
+    const category = new Category(props);
+
+    //  Assert
     expect(category.name).toBe("Movie");
+    expect(category.description).toBe("Movie description");
+    expect(category.isActive).toBe(true);
+    expect(category.createdAt).toBe(props.createdAt);
   });
 });
